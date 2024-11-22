04a17d458d701412c8ed73bb5668577cfa72b906
rohitnaikawadi26 <rohitnaikawadi2610@gmail.com>
2024-11-22 10:16:53 +0530

    Laravel HMS

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="about" class="section  wow fadeIn">
+    <div class="container">
+      <div class="heading">
+         <span class="icon-logo"><img src="images/icon-logo.png" alt="#"></span>
+         <h2>The Specialist Clinic</h2>
+      </div>
+      <!-- end title -->
+      <div class="row">
+         <div class="col-md-6">
+            <div class="message-box">
+               <h4>What We Do</h4>
+               <h2>Clinic Service</h2>
+               <p class="lead">Our clinic is committed to providing exceptional healthcare services to individuals and families, with a strong emphasis on delivering compassionate, patient-centered care. We understand that each patient’s health needs are unique, which is why we offer a broad spectrum of services designed to address a variety of medical concerns. Our team of skilled and experienced doctors, nurses, and medical professionals are dedicated to supporting your health journey through comprehensive treatment plans tailored specifically to you.
+
+               We provide general health check-ups, preventive care, routine screenings, and diagnostics, helping to detect potential health issues early. In addition, our clinic specializes in the management of chronic conditions, offering ongoing support for diseases such as diabetes, hypertension, and asthma. Whether you're seeking care for acute illnesses, minor injuries, or complex conditions, our team is equipped to provide effective solutions.
+
+               Our clinic is equipped with state-of-the-art medical technology, ensuring the accurate diagnosis and timely treatment of a wide range of conditions. From laboratory testing and imaging services to modern treatment techniques, we are committed to using the best tools and approaches to meet your healthcare needs. We also focus on promoting wellness and healthy lifestyles through counseling, education, and resources, empowering our patients to take control of their health. </p>
+            </div>
+            <!-- end messagebox -->
+         </div>
+         <!-- end col -->
+         <div class="col-md-6">
+            <div class="post-media wow fadeIn">
+               <img src="images/about_03.jpg" alt="" class="img-responsive">
+               <a href="http://www.youtube.com/watch?v=nrJtHemSPW4" data-rel="prettyPhoto[gal]" class="playbutton"><i class="flaticon-play-button"></i></a>
+            </div>
+            <!-- end media -->
+         </div>
+         <!-- end col -->
+      </div>
+
+   </div>
+   <!-- end container -->
+</div>
+@endsection
---
+++ resources/views/docter.blade.php
@@ -0,0 +1,164 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="doctors" class="parallax section db" data-stellar-background-ratio="0.4" style="background:#fff;" data-scroll-id="doctors" tabindex="-1">
+    <div class="container">
+  
+     <div class="heading">
+           <span class="icon-logo"><img src="images/icon-logo.png" alt="#"></span>
+           <h2>The Specialist Clinic</h2>
+        </div>
+  
+        <div class="row dev-list text-center">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.2s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_01.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Soren Bo Bostian</h3>
+                        <small>Clinic Owner</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hello guys, I am Soren from Sirbistana. I am senior art director and founder of Violetta.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+  
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hello, I’m Dr. Gaurav from HealthFirst, a senior physician and the founder of WellnessHub, dedicated to providing holistic care for all patients.p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hi, I’m Dr.Sunita from MediCare, a senior consultant, and the founder of LifeLine Health, focused on delivering personalized healthcare solutions.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Greetings, I’m Dr. Diksha from VitalCare, a leading specialist in neurology and the founder of NeuroCure, committed to advancing brain health and patient well-being.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hello everyone, I am Dorciotr from Artify, a senior creative strategist and founder of VisioCraft.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hello, I’m Dr. Monika from HealthFirst, a senior physician and the founder of WellnessHub, dedicated to providing holistic care for all patients.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.4s; animation-name: fadeIn;">
+                <div class="widget clearfix">
+                    <img src="images/doctor_02.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Bryan Saftler</h3>
+                        <small>Internal Diseases</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Hi, I’m Dr. Neha from MediCare, a senior consultant, and the founder of LifeLine Health, focused on delivering personalized healthcare solutions.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+  
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 wow fadeIn">
+                <div class="widget clearfix">
+                    <img src="images/doctor_03.jpg" alt="" class="img-responsive img-rounded">
+                    <div class="widget-title">
+                        <h3>Matthew Bayliss</h3>
+                        <small>Orthopedics Expert</small>
+                    </div>
+                    <!-- end title -->
+                    <p>Greetings, I’m Dr. Kirti from VitalCare, a leading specialist in neurology and the founder of NeuroCure, committed to advancing brain health and patient well-being.</p>
+  
+                    <div class="footer-social">
+                        <a href="#" class="btn grd1"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-github"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn grd1"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div><!--widget -->
+            </div><!-- end col -->
+  
+        </div><!-- end row -->
+    </div><!-- end container -->
+  </div>
+
+@endsection
